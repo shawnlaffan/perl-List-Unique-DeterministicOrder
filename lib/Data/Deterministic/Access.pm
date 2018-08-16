92ab25212601c614ea8f3e22755cf1f41b69c1f2
Shawn Laffan <shawnlaffan@gmail.com>
2018-08-16 20:37:37 +1000

    Add module packaging files

--- lib/Data/Deterministic/Access.pm
+++ lib/Data/Deterministic/Access.pm
@@ -129,3 +129,240 @@ sub _paranoia {
     
     return 1;
 }
+
+1;
+
+=head1 NAME
+
+Data::Deterministic::Access - Store keys with deterministic order based on insertions and deletions
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+=head1 SYNOPSIS
+
+This module provides a structure to store a set
+of keys and be able to access (mostly delete)
+them both by key name and by index.
+
+The algorithm used inserts at the end, but
+swaps keys around on deletion.  Hence it is
+deterministic and repeatable providing the
+sequence of insertion and deletions is also
+repeated.  
+
+The system does not store any contents with the keys,
+so it is more of a "set" structure, but one that
+does not do intersections, unions and the like.
+
+So why would one want this in the first place?
+In my case it is for a series of random selections
+where keys are extracted from a pool of keys,
+and sometimes inserted.  The randomisations should
+be the same for the same given PRNG sequence.
+Binary searches over sorted lists proved very
+effective for a while, but bottlenecks started
+to manifest when the data sets became
+much larger and the number of lists
+became both abundant and lengthy.
+
+
+    use Data::Deterministic::Access;
+
+    my $foo = Data::Deterministic::Access->new(
+        data => [qw /foo bar quux fetangle/]
+    );
+    
+    print $foo->keys;
+    #  foo bar quux fetangle
+    
+    $foo->delete ('bar')
+    print $foo->keys;
+    #  foo fetangle quux 
+    
+    print $foo->get_key_at_pos(2);
+    #  quux
+    print $foo->get_key_at_pos(20);
+    #  undef
+    
+    $foo->push ('bardungle')
+    print $foo->keys;
+    #  foo fetangle quux bardungle
+
+    #  duplicates are stored only once,
+    #  just like with a normal hash
+    $foo->push ('fetangle')
+    print $foo->keys;
+    #  foo fetangle quux bardungle
+    
+    print $foo->exists ('gelert');
+    #  false
+    
+    print $foo->pop;
+    #  bardungle
+    
+
+=head1 METHODS
+
+Note that most methods take a single argument
+(if any), so while the method names look
+hash-like, this is purely cosmetic.
+
+=head2 new
+
+Create a new object.
+Optionally pass data using the data
+keyword.  Duplicate keys are
+stored once only.  
+
+=cut
+
+=head2 delete
+
+Deletes the key passed as an argument.
+
+=cut
+
+=head2 exists
+
+True or false for if the key exists.
+
+=cut
+
+=head2 get_key_at_pos
+
+Returns the key at some position.
+
+$foo->get_key_at_pos(5);
+
+=cut
+
+=head2 get_key_pos
+
+Returns the position of a key.
+
+$foo->get_key_pos('quux');
+
+=cut
+
+=head2 keys
+
+Returns the list of keys in list context,
+and the number of keys in scalar context.
+
+=cut
+
+=head2 pop
+
+Removes and returns the last key in the list.
+
+=cut
+
+=head2 push
+
+Appends the specified key to the end of the list,
+unless it is already in the set.
+
+=cut
+
+=head2 splice
+
+Removes a single key from the set.
+
+=cut
+
+
+=head1 AUTHOR
+
+Shawn Laffan, C<< <shawnlaffan at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-data-deterministic-access at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-Deterministic-Access>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Data::Deterministic::Access
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Data-Deterministic-Access>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Data-Deterministic-Access>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Data-Deterministic-Access>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Data-Deterministic-Access>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+The algorithm used is from
+L<https://stackoverflow.com/questions/5682218/data-structure-insert-remove-contains-get-random-element-all-at-o1/5684892#5684892>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2018 Shawn Laffan 
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
